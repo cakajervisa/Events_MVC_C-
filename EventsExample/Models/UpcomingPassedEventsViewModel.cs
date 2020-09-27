942a3d13c3d4c9f7933cd2b4c45c1199a32af19c
ervisaaa <ervisa.cakaj@gmail.com>
2020-09-27 23:51:59 +0200

    1

---
+++ EventsExample/Models/UpcomingPassedEventsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EventsExample.Models
+{
+    using System.Collections.Generic;
+
+    public class UpcomingPassedEventsViewModel
+    {
+        public IEnumerable<EventViewModel> UpcomingEvents { get; set; }
+        
+        public IEnumerable<EventViewModel> PassedEvents { get; set; }
+    }
+}
---
+++ EventsExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventsExample.Startup))]
+namespace EventsExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
